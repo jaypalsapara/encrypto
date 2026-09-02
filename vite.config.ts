e3977c4cfb707a0f76f3baf9217a2b3a22435ba2
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-09-02 15:45:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/encrypto/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
